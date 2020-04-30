c5822b3d4957a4f63af72c5123ece145909f0b1e
Florida Ahado <akou.ahado@pcc.edu>
2020-04-29 17:16:39 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld2/HelloWorld2.java
+++ src/edu/pcc/cis/helloworld2/HelloWorld2.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld2;
 
+/**
+ * Created by Florida Ahado on 4/29/2020.
+ */
 public class HelloWorld2 {
+    public static void main (String[] args){
+        System.out.println("Hello, World!");
+    }
 }
